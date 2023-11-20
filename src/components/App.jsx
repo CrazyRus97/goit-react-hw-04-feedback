1a0ee270fa3f16f93d5ee6fa4044004982857c03
Fedorenko Ruslan <130165394+CrazyRus97@users.noreply.github.com>
2023-11-20 17:55:28 +0200

    start

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
+import { useState } from 'react';
+
+import { FeedbackOptions } from './feedbackOptions/FeedbackOptions';
+import { Layout } from './layout/Layout';
+import { Section } from './section/Section';
+import { Statistics } from './statistics/Statistics';
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const leaveFeedback = e => {
+    //console.log(e); // good
+    switch (e) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = () =>
+    Math.round((good * 100) / countTotalFeedback());
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={leaveFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={countTotalFeedback()}
+          positivePercentage={countPositiveFeedbackPercentage()}
+        ></Statistics>
+      </Section>
+    </Layout>
   );
-};
+}
+
+// npm i prop-types
+// npm i react-toastify
+// npm i nanoid
+// npm i react
+// npm i react-icons
+// npm i yup
+// npm i formik
+// npm i styled-components
+// npm i modern-normalize
+
---
+++ src/components/notification/Notification.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import css from './Notification.module.css'
+
+export const Notification = ({ msg }) => {
+  return <h2 className={css.message}>{msg}</h2>;
+};
+
